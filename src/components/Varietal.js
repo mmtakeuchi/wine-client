dcb5e0ece29adc2b3db5454fd25432d31c0f3f57
mmtakeuchi <mikemtaki@gmail.com>
2021-04-15 00:01:12 -0700

    added div to Varietal to show if there are no wines under the varietal type

--- src/components/Varietal.js
+++ src/components/Varietal.js
@@ -9,27 +9,30 @@ export class Varietal extends Component {
   };
 
   render() {
-    console.log(this);
     const { varietals } = this.props;
-    console.log(varietals);
 
     const varietal = varietals.find(
       (varietal) => varietal.id === parseInt(this.props.match.params.id)
     );
 
-    console.log(varietal);
-
     if (varietal && varietal.wines) {
       return (
         <div>
           <h2>{varietal.name}</h2>
-          <ul>
-            {varietal.wines.map((wine) => (
-              <li key={wine.id}>
-                <Link to={`/wines/${wine.id}`}>{wine.brand}</Link>
-              </li>
-            ))}
-          </ul>
+          {varietal.wines.length > 0 ? (
+            <ul>
+              {varietal.wines.map((wine) => (
+                <li key={wine.id}>
+                  <Link to={`/wines/${wine.id}`}>{wine.brand}</Link>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <div>
+              <div>No wines recorded from {varietal.name}.</div>
+            </div>
+          )}
+
           <br />
           <button onClick={() => this.props.history.push("/varietals")}>
             Back
