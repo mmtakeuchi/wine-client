41e21f38e5bd5d17da6d9d5d9958650e2793eae7
mmtakeuchi <mikemtaki@gmail.com>
2021-04-25 21:38:46 -0700

    test

--- src/actions/varietalActions.js
+++ src/actions/varietalActions.js
@@ -2,10 +2,12 @@ import axios from "axios";
 import { API_ROOT } from "../apiRoot";
 
 export const getVarietals = () => {
+  console.log(`${API_ROOT}/varietals`);
   return (dispatch) => {
     axios
       .get(`${API_ROOT}/varietals`)
       .then((resp) => {
+        console.log(resp);
         return dispatch({ type: "GET_VARIETALS", varietals: resp.data });
       })
       .catch((errors) => {
